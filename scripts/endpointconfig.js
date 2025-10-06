fd712cc66b08995861cc9b14c492399723c5590c
Subha <subhachauhan9569@gmail.com>
2025-10-06 16:44:46 +0530

    added changes

---
+++ scripts/endpointconfig.js
@@ -0,0 +1,9 @@
+function getAEMPublish() {
+  return 'https://publish-p8803-e21945.adobeaemcloud.com';
+}
+
+function getAEMAuthor() {
+  return 'https://author-p8803-e21945.adobeaemcloud.com';
+}
+
+export { getAEMPublish, getAEMAuthor };
